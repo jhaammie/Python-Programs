1ac4623570272f4a85dd7273447f5d2279cd1035
jhaan <jhaammie@gmail.com>
2025-04-21 15:10:09 +0200

    HEHEHEHEHHEHEHEHEHEHEHHE

--- Gymnasium/Main.py
+++ Gymnasium/Main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from hoohoohee import GetNearestSchools, GetDataForSchools
+from hoohoohee import GetNearestSchools, GetDataForSchools, GetGymnasiumWithinRadius
 
 app = Flask(__name__)  # Creates an instance of app
 CORS(app)
@@ -19,7 +19,7 @@ def GetNearestGymnasium():
 
     for school in schools:
         lst.append(school[0])
-    DataList = GetDataForSchools(lst)
+    DataList = GetDataForSchools(lst, None, None)
     print(DataList[0])
     list = []
     for school in DataList:
@@ -40,6 +40,48 @@ def GetNearestGymnasium():
         list.append(d)
     return list
 
+@app.route('/gymnasium-within-radius', methods=["POST"])
+def GymnasiumWithinRadius():
+    content = request.json
+    a = content["latitude"]
+    b = content["longitude"]
+    radius = content["radius"]
+    sortby = content["sortBy"]
+    sortOrder = content["sortOrder"]
+    minMerit = content["minMerit"]
+    maxMerit = content["maxMerit"]
+    program = content["program"]
+    lst = []
+    schools = GetGymnasiumWithinRadius(a, b, radius)
+#    for a in range(0, len(schools)):
+ #       PutMeInLst = schools[a][0]
+  #      lst.append(PutMeInLst)
+
+    for school in schools:
+        lst.append(school[0])
+    DataList = GetDataForSchools(lst, sortby, sortOrder, minMerit, maxMerit, program)
+    if len(DataList) > 0:
+        print(DataList[0])
+
+
+    list = []
+    for school in DataList:
+        d = {"Year":school[1],
+              "Is_Preliminary": school[2],
+              "Kommun": school[3],
+              "Name":school[4],
+               "Organisitionsform":school[5],
+               "Studievagskod":school[6],
+               "Studievag":school[7],
+               "Antagningsgrans":school[8],
+               "Median":school[9],
+              "Antal_platser":school[10],
+              "Antagna":school[11],
+              "Reserver":school[12],
+              "Lediga_platser":school[13]
+             }
+        list.append(d)
+    return list
 
 
 # Get the data for those schools (l.22)
--- Gymnasium/hoohoohee.py
+++ Gymnasium/hoohoohee.py
@@ -83,11 +83,19 @@ def GetNearestSchools(latitude, longitude,count):
     return data
 
 
-def GetDataForSchools(lst):
+def GetDataForSchools(lst, sortby, sortOrder, minMerit=0, maxMerit=1000, program=None):
+
     placeholders = ",".join(f"'{name}'" for name in lst)
     data = []
     try:
-        query = f"select * from gymnasium where skola in ({placeholders})"
+        if sortby is None:
+            query = f"select * from gymnasium where skola in ({placeholders})"
+        else:
+            query = f"select * from gymnasium where skola in ({placeholders})"
+            if program is not None:
+                query = f"{query} and studieväg ilike '{program}%' "
+            query = f"{query} and antagningsgräns between {minMerit} and {maxMerit} order by {sortby} {sortOrder}"
+
         connection = __GetdbConn()
         cursor = connection.cursor()
         cursor.execute(query)
@@ -101,6 +109,25 @@ def GetDataForSchools(lst):
             connection.close()
         # print('Database connection closed.')
     return data
+
+def GetGymnasiumWithinRadius(latitude, longitude, radius):
+    data = []
+    try:
+        query = f"select name, ST_DistanceSphere(ST_MakePoint({longitude}, {latitude}), ST_MakePoint(school.longitude, school.latitude)) / 1000 as distance_in_km from school where ST_DistanceSphere(ST_MakePoint({longitude}, {latitude}), ST_MakePoint(school.longitude, school.latitude)) / 1000 <= {radius} order by distance_in_km asc;"
+        connection = __GetdbConn()
+        cursor = connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+        # print('Database connection closed.')
+    return data
+
 
 """print(GetListOfSchoolNames(0, 2))
 print(InsertSchool("S:t Botvids Gymnasium", "33.8", "88.99"))
