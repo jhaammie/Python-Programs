75dce5dd8134d06fb7c1dc47b7ad82f9ccb6f9d6
Nisha <gnisharn@gmail.com>
2025-05-04 00:23:17 +0530

    cors fix

--- flask_hello_world/main.py
+++ flask_hello_world/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://hw.edigistay.com"}})
 
 @app.route('/hello')
 def hello_world():
