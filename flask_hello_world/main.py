f618cd0ec6634a189c270f89e9ac6dbe41d45d5f
Nisha <gnisharn@gmail.com>
2025-05-04 00:46:36 +0530

    path modified to /api

--- flask_hello_world/main.py
+++ flask_hello_world/main.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://hw.edigistay.com"}})
 
-@app.route('/hello')
+@app.route('/api/hello')
 def hello_world():
     return 'Hello, World from Python Backend!'
 
