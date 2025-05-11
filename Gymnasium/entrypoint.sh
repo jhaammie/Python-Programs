593d10975cb083e66e326d6d92b2bf991f8f123e
Nisha <gnisharn@gmail.com>
2025-05-12 00:52:19 +0530

    adding logs to debug

--- Gymnasium/entrypoint.sh
+++ Gymnasium/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-
+echo "Starting entrypoint script..."
+echo "Environment variable API_BASE_URL: ${API_BASE_URL}"
+echo "Creating config.js file..."
 cat <<EOF > /usr/share/nginx/html/config.js
 window.CONFIG = {
   API_BASE_URL: "${API_BASE_URL}"
 };
 EOF
 
+echo "Config file created..."
+
 exec nginx -g 'daemon off;'
+echo "Nginx started..."
+echo "Entrypoint script completed."
+# This script creates a config.js file in the Nginx HTML directory with the API_BASE_URL environment variable.
+# It then starts the Nginx server in the foreground.
